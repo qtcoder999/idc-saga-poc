1546018d91fee8d2e7018f8fe20b769987c0956e
Paras Anand <qtcoder@gmail.com>
2018-12-24 00:33:48 +0530

    implemented redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,17 @@
-import {createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from '../reducers/rootReducer';
+
 import mySaga from '../components/sagas'
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware()
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
+
 // then run the saga
 sagaMiddleware.run(mySaga)
 
