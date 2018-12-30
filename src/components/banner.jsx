79fee94b225fb21439f698667ad978b8c7b09ad6
paras  anand <paras.anand@idc.bedbath.com>
2018-12-30 16:21:25 +0530

    css updates

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './banner.css';
+import './banner.min.css';
 
 class Banner extends Component {
 
