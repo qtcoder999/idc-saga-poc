2ba2ddd2fe72624cecd72503c9caa9eab8622fd3
paras  anand <paras.anand@idc.bedbath.com>
2019-01-01 15:43:04 +0530

    heroku-git issues : fixing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Bannerdetails from './containers/bannerdetails1/bannerdetails1';
-import CounterDetails from './containers/counterdetails/counterdetails'
+import CounterDetails from './containers/counterdetails1/counterdetails1'
 
 class App extends Component {
   render() {
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux-immutable';
 import imageCounterReducer from './containers/bannerdetails1/reducer';
-import counterReducer from './containers/counterdetails/reducer'
+import counterReducer from './containers/counterdetails1/reducer'
 
 export default combineReducers({
     imageCounterReducer,
